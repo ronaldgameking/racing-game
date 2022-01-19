756de646072c87cd1defa002ed9804dadf2c239b
Ronald <kpn.nl.rs@gmail.com>
2022-01-19 21:55:13 +0100

    Added verbose Logging & Level filter

--- Assets/Scripts/Util/Logger.cs
+++ Assets/Scripts/Util/Logger.cs
@@ -6,16 +6,35 @@ using UnityEditor;
 /// </summary>
 public class Logger
 {
+    public enum DebugLevel
+    {
+        Verbose = 0,
+        Info,
+        Warning,
+        Error,
+        Exception,
+        Fatal
+    }
     public static bool Debugging = true;
-    
+    public static DebugLevel Level = DebugLevel.Info;
+
+    /// <summary>
+    /// Verbose log
+    /// </summary>
+    /// <param name="message"></param>
+    public static void LogVerbose(object message)
+    {
+        if (Debugging && Level == DebugLevel.Verbose)
+            Debug.Log(message);
+    }
     public static void Log(object message)
     {
-        if (Debugging)
+        if (Debugging && Level <= DebugLevel.Info)
             Debug.Log(message);
     }
     public static void LogWarning(object message)
     {
-        if (Debugging)
+        if (Debugging && Level <= DebugLevel.Warning)
             Debug.LogWarning(message);
     }
 
