4bf6792841bd0545991cdc397aee2ed6e7773eaf
Reliii <rensvanderlit@gmail.com>
2022-01-25 10:50:41 +0100

    s

---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour 
+{
+    private const float Max_Speed_Angle = 0;
+    private const float Zero_Speed_Angle = 180;
+
+    private Transform NeedleTranform;
+
+    private float SpeedMax;
+    private float Speed;
+
+    private void Awake() {
+        NeedleTranform = transform.Find("Needle");
+
+        Speed = 0f;
+        SpeedMax = 200f;
+    }
+
+    private void Update() 
+    {
+        HandlePlayerInput();
+
+        NeedleTranform.eulerAngles = new Vector3(0, 0, GetSpeedRotation());
+    }
+
+    private void HandlePlayerInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            float acceleration = 50f;
+            Speed += acceleration * Time.deltaTime;
+        }
+        else
+        {
+            float deceleration = 100f;
+            Speed -= deceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            float brakeSpeed = 100f;
+            Speed -= brakeSpeed * Time.deltaTime;
+        }
+
+        Speed = Mathf.Clamp(Speed, 0f, SpeedMax);
+    }
+
+    private float GetSpeedRotation() 
+    {
+        float totalAngleSize = Zero_Speed_Angle - Max_Speed_Angle;
+
+        float speedNormalized = Speed / SpeedMax;
+
+        return Zero_Speed_Angle - speedNormalized * totalAngleSize;
+    }
+}
